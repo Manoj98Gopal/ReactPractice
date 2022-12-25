cd1589b3f65825a732cee78738771cb46681f367
manoj98gopal <manoj98gopal@gmail.com>
2022-12-25 15:06:14 +0530

    React basic code

---
+++ project/App.js
@@ -0,0 +1,21 @@
+
+ const heading1 = React.createElement("h1",{id:"title"},"Hello this is React!");
+ const pharagraf1 = React.createElement("p",{id:"phra"},"Hello this is React! Hello this is React Hello this is React");
+
+
+ const heading2 = React.createElement("h2",{id:"title"},"This heading is 2");
+ const pharagraf2 = React.createElement("p",{id:"phra"},"Hello this is React! Hello this is React Hello this is React");
+
+
+ const heading3 = React.createElement("h3",{id:"title"},"This heading is 3");
+
+ const container = React.createElement("div",{id:"container"},[heading1,pharagraf1])
+
+ const container1 = React.createElement("div",{id:"container1"},[heading1,pharagraf2])
+
+
+
+ const root = ReactDOM.createRoot(document.getElementById("root"))
+
+ root.render([container,container1,heading3])
+
