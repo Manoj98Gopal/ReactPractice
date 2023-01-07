f892858509a11de921ccf2f38a5a8b0992964e56
manoj98gopal <manoj98gopal@gmail.com>
2023-01-07 22:58:08 +0530

    Food header added

--- project/App.js
+++ project/App.js
@@ -1,27 +1,101 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Logo from "./public/logo.jpeg";
-import userIcon from "./public/userIcon.png";
+
+
+const Title = () => {
+
+  return (
+  <a href="/">
+  <img 
+      className="logo"
+      alt="Logo"
+  src="https://image.similarpng.com/very-thumbnail/2021/09/Good-food-logo-design-on-transparent-background-PNG.png"/></a>)
+}
 
 const Header = () => {
   return (
-    <div style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
-      <div>
-        <img src={Logo} style={{ borderRadius: "40px", width: "65px",position:"relative",left:"20px" }} />
+    
+      <div className="header">
+        <div>
+        <Title />
+        </div>
+        <div className="nav-list">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Service</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+        </div>
+        
+
       </div>
-      <div style={{width:"100%",position:"relative",left:"200px"}}>
-        <input
-          style={{ width: "70%", height: "20px", borderRadius: "10px",padding:"8px 14px 8px 32px",fontSize:"17px" }}
-          placeholder="Search here ...."
-        />
-      </div>
-      <div>
-        <img src={userIcon} style={{ width: "90px",position:"relative",right:"35px",top:"10px" }} />
-      </div>
-    </div>
+    
   );
 };
 
+
+const data = {
+    image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYgv_7WcHGDMSb2j_ZbjdqXWr9s0UEumDMag&usqp=CAU",
+    name : "Burgar King",
+    stars : "4.8",
+}
+
+const RestrauntCard = () => {
+
+  return (
+    <div className="food-card">
+      <img src={data.image} alt="img"/>
+      <h2>{data.name}</h2>
+      <h3>{data.stars} stars</h3>
+    </div>
+  )
+}
+
+const Body = () => {
+
+  return(
+    <div className="food-card-list">
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+        <RestrauntCard />
+    </div>
+  )
+}
+
+const Footr = () => {
+
+  return(
+    <h3>I am footer</h3>
+  )
+}
+
+
+const Applayout = () => {
+
+  return(
+    <>
+    <Header />
+    <Body/>
+    <Footr/>
+    </>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Header />);
+root.render(<Applayout />);
