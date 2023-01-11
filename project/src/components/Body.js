7c841745da007c02771e65bc86912b1c28ff93a5
manoj98gopal <manoj98gopal@gmail.com>
2023-01-11 22:45:57 +0530

    Search bar is added

--- project/src/components/Body.js
+++ project/src/components/Body.js
@@ -1,15 +1,50 @@
+import { useState } from "react";
 import RestrauntCard from "./RestrauntCard";
 import { cardData } from "../Constants";
 
 const Body = () => {
+  const [search, setSearch] = useState("");
+  const [cardInfo, setCardData] = useState(cardData);
 
-    return(
-      <div className="food-card-list">
-        {cardData.map((curObj) => {
-          return <RestrauntCard  {...curObj.data}/>     
-        })}
+  return (
+    <div>
+      <div>
+        <input
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          placeholder="Search here ...."
+          className="searchBar"
+        />
+        <button
+          onClick={() => {
+            const data = filterFunction(search, cardInfo);
+            setCardData(data);
+          }}
+        >
+          Search
+        </button>
       </div>
-    )
-  }
+      <div className="food-card-list">
+        {cardData
+          .filter((cur) => {
+            if (search == "") {
+              return cur;
+            } else if (
+              cur.data.name
+                .toLocaleLowerCase()
+                .includes(search.toLocaleLowerCase())
+            ) {
+              return cur;
+            }
+          })
+          .map((curObj) => {
+            return <RestrauntCard {...curObj.data} key={curObj.data.id} />;
+          })}
+      </div>
+    </div>
+  );
+};
 
-  export default Body;
+export default Body;
