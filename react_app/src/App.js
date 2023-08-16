14b70c634d7f0a147ac99d00cec1b0619d312787
manoj98gopal <manoj98gopal@gmail.com>
2023-08-16 15:00:42 +0530

    create react demo app

---
+++ react_app/src/App.js
@@ -0,0 +1,14 @@
+import { Card, Typography } from "antd";
+const {Title} = Typography
+
+function App() {
+  return (
+    <>
+    <Card>
+      <Title level={2}>Main component !</Title>
+    </Card>
+    </>
+  );
+}
+
+export default App;
